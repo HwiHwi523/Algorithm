515210bb8050350479ae41bc90c670d1b647b8be
윤재휘 <yjh200523@naver.com>
2024-01-26 13:18:28 +0900

    [refactor] 메모리 개선 및 메서드 구조 변경

--- Baekjoon/BOJ_2094_강수량.py
+++ Baekjoon/BOJ_2094_강수량.py
@@ -4,46 +4,31 @@
 import sys
 
 
-def get_segment(precip):
-    segments = [0] * (len(precip) << 1)
-
-    for i in range(len(precip), len(segments)):
-        segments[i] = precip[i - len(precip)]
-
-    for i in range(len(precip) - 1, 0, -1):
-        segments[i] = max(segments[i << 1], segments[(i << 1) + 1])
-
-    return segments
-
-
-def query(n, segment, i, j):
-    if i == j:
-        return 0
-
-    i += n
-    j += n
-    max_p = 0
-
-    while i < j:
-        if i & 1:
-            max_p = max(max_p, segment[i])
-            i += 1
-
-        if j & 1:
-            j -= 1
-            max_p = max(max_p, segment[j])
-
-        i >>= 1
-        j >>= 1
-
-    return max_p
-
-
 def solution():
+    def query(i, j):
+        if i == j:
+            return 0
+
+        max_p = 0
+
+        while i < j:
+            if i & 1:
+                max_p = max(max_p, segment[i])
+                i += 1
+
+            if j & 1:
+                j -= 1
+                max_p = max(max_p, segment[j])
+
+            i >>= 1
+            j >>= 1
+
+        return max_p
+
     def idx_of(p_year):
         begin = 0
-        end = len(years) - 1
-        idx, found = len(years), False
+        end = n - 1
+        idx, found = n, False
 
         while begin <= end:
             mid = (begin + end) >> 1
@@ -57,20 +42,21 @@ def solution():
             else:
                 begin = mid + 1
 
-        return idx, found
+        return idx + n, found
 
     sys_input = sys.stdin.readline
 
     n = int(sys_input())
-    years = []
-    precip = []
+    segment = [0] * (n << 1)
+    years = [0] * n
 
     for i in range(n):
         year, p = map(int, sys_input().split())
-        years.append(year)
-        precip.append(p)
+        years[i] = year
+        segment[i + n] = p
 
-    segment = get_segment(precip)
+    for i in range(n - 1, 0, -1):
+        segment[i] = max(segment[i << 1], segment[(i << 1) + 1])
 
     for _ in range(int(sys_input())):
         y, x = map(int, sys_input().split())
@@ -78,30 +64,28 @@ def solution():
         y_idx, y_found = idx_of(y)
         x_idx, x_found = idx_of(x)
         between_p = query(
-            n,
-            segment,
             y_idx + (1 if y_found else 0),
             x_idx
         )
 
         if y_found and x_found:
-            if x_idx - y_idx == x - y and between_p < precip[x_idx] <= precip[y_idx]:
+            if x_idx - y_idx == x - y and between_p < segment[x_idx] <= segment[y_idx]:
                 print('true')
 
-            elif precip[y_idx] < precip[x_idx] or between_p >= precip[x_idx]:
+            elif segment[y_idx] < segment[x_idx] or between_p >= segment[x_idx]:
                 print('false')
 
             else:
                 print('maybe')
 
         elif y_found:
-            if precip[y_idx] <= between_p:
+            if segment[y_idx] <= between_p:
                 print('false')
             else:
                 print('maybe')
 
         elif x_found:
-            if between_p >= precip[x_idx]:
+            if between_p >= segment[x_idx]:
                 print('false')
             else:
                 print('maybe')
