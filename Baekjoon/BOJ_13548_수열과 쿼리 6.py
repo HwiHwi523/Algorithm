6e26ac6ef4451c4ea28a8dfb1cbf46d2b7c646a6
윤재휘 <yjh200523@naver.com>
2024-01-28 17:49:05 +0900

    [refactor] mo's, 오프라인 쿼리

--- Baekjoon/BOJ_13548_수열과 쿼리 6.py
+++ Baekjoon/BOJ_13548_수열과 쿼리 6.py
@@ -4,42 +4,6 @@
 import sys
 
 
-class Node:
-    def __init__(self, count):
-        self.count = count
-        self.nums = set()
-        self.prev = self
-        self.next = self
-
-
-def increase(before, after, num):
-    after.nums.add(num)
-    if len(after.nums) == 1:
-        after.prev = before.prev
-        after.next = before
-        before.prev.next = after
-        before.prev = after
-
-    before.nums.remove(num)
-    if len(before.nums) == 0:
-        before.prev.next = before.next
-        before.next.prev = before.prev
-
-
-def decrease(before, after, num):
-    after.nums.add(num)
-    if len(after.nums) == 1:
-        after.prev = before
-        after.next = before.next
-        before.next.prev = after
-        before.next = after
-
-    before.nums.remove(num)
-    if len(before.nums) == 0:
-        before.prev.next = before.next
-        before.next.prev = before.prev
-
-
 def solution():
     sys_input = sys.stdin.readline
 
@@ -54,41 +18,47 @@ def solution():
 
     max_num = max(nums)
     counts = [0] * (max(max_num, n) + 1)
-    nodes = [Node(i) for i in range(max(max_num, n) + 1)]
+    cand_by_count = [set() for _ in range(len(counts))]
+    cand_by_count[0].update(set(nums))
 
-    root = Node(-1)
-    root.prev = root.next = nodes[0]
-    nodes[0].prev = nodes[0].next = root
-    nodes[0].nums.update(set(nums))
-
-    left, right = 0, -1
+    left, right, max_count = 0, -1, 0
     answer = [0] * q
     for idx, a, b in queries:
         while right < b:
             right += 1
             count = counts[nums[right]]
-            increase(nodes[count], nodes[count + 1], nums[right])
             counts[nums[right]] += 1
+            cand_by_count[count].remove(nums[right])
+            cand_by_count[count + 1].add(nums[right])
+            max_count = max(max_count, counts[nums[right]])
 
         while b < right:
             count = counts[nums[right]]
-            decrease(nodes[count], nodes[count - 1], nums[right])
+            cand_by_count[count].remove(nums[right])
+            cand_by_count[count - 1].add(nums[right])
+            if max_count == count and not cand_by_count[count]:
+                max_count -= 1
             counts[nums[right]] -= 1
             right -= 1
 
         while left < a:
             count = counts[nums[left]]
-            decrease(nodes[count], nodes[count - 1], nums[left])
+            cand_by_count[count].remove(nums[left])
+            cand_by_count[count - 1].add(nums[left])
+            if max_count == counts[nums[left]] and not cand_by_count[count]:
+                max_count -= 1
             counts[nums[left]] -= 1
             left += 1
 
         while a < left:
             left -= 1
             count = counts[nums[left]]
-            increase(nodes[count], nodes[count + 1], nums[left])
             counts[nums[left]] += 1
+            cand_by_count[count].remove(nums[left])
+            cand_by_count[count + 1].add(nums[left])
+            max_count = max(max_count, counts[nums[left]])
 
-        answer[idx] = root.next.count
+        answer[idx] = max_count
 
     print(*answer, sep='\n')
 
